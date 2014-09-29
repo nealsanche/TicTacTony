cca3d7c83b7a94b63219b3b8d141b17c0844b16d
Neal Sanche <neal.sanche@robotsandpencils.com>
2014-09-29 15:40:50 -0600

    Initial Commit

---
+++ app/src/main/java/com/tony/tictactoegame/MainActivity.java
@@ -0,0 +1,190 @@
+package com.tony.tictactoegame;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity
+{
+
+    private TicTacToeGame mGame;
+
+    private Button mBoardButtons[];
+
+    private TextView mInfoTextView;
+    private TextView mPlayerCount;
+    private TextView mTiesCount;
+    private TextView mAndroidCount;
+
+    private int mPlayerCounter = 0;
+    private int mTiesCounter = 0;
+    private int mAndroidCounter = 0;
+
+    private boolean mPlayerFirst = true;
+    private boolean mGameOver = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mGame = new TicTacToeGame();
+
+        mBoardButtons = new Button[mGame.GetBoardSize() * mGame.GetBoardSize()];
+        mBoardButtons[0] = (Button) findViewById(R.id.one);
+        mBoardButtons[1] = (Button) findViewById(R.id.two);
+        mBoardButtons[2] = (Button) findViewById(R.id.three);
+        mBoardButtons[3] = (Button) findViewById(R.id.four);
+        mBoardButtons[4] = (Button) findViewById(R.id.five);
+        mBoardButtons[5] = (Button) findViewById(R.id.six);
+        mBoardButtons[6] = (Button) findViewById(R.id.seven);
+        mBoardButtons[7] = (Button) findViewById(R.id.eight);
+        mBoardButtons[8] = (Button) findViewById(R.id.nine);
+
+
+        mInfoTextView = (TextView) findViewById(R.id.Information);
+        mPlayerCount = (TextView) findViewById(R.id.count);
+        mTiesCount = (TextView) findViewById(R.id.ties_counter);
+        mAndroidCount = (TextView) findViewById(R.id.Comp_Counter);
+
+        mPlayerCount.setText(Integer.toString(mPlayerCounter));
+        mTiesCount.setText(Integer.toString(mTiesCounter));
+        mAndroidCount.setText(Integer.toString(mAndroidCounter));
+
+        StartNewGame();
+    }
+
+    private void StartNewGame()
+    {
+        mGame.ClearBoard();
+
+        for(int i = 0; i < mBoardButtons.length;++i )
+        {
+            mBoardButtons[i].setText("");
+            mBoardButtons[i].setEnabled(true);
+            mBoardButtons[i].setOnClickListener(new ButtonClickListener(i));
+        }
+
+        if(mPlayerFirst)
+        {
+            mInfoTextView.setText(R.string.first_human);
+            mPlayerFirst = false;
+        }
+        else
+        {
+            mInfoTextView.setText(R.string.turn_computer);
+            int move = mGame.GetComputerMove();
+
+            SetMove(mGame.GetComputerPlayer(), move);
+            mPlayerFirst = true;
+
+        }
+
+    }
+
+
+    private class ButtonClickListener implements View.OnClickListener
+    {
+        int location;
+
+        public ButtonClickListener(int location)
+        {
+            this.location = location;
+        }
+
+        public void onClick(View view)
+        {
+            if(!mGameOver)
+            {
+                if(mBoardButtons[location].isEnabled())
+                {
+                    SetMove(mGame.GetHumanPlayer(), location);
+
+                    TicTacToeGame.TypeOfWinner winner = mGame.CheckForWinner();
+
+                    if(winner == TicTacToeGame.TypeOfWinner.GameNotOver)
+                    {
+                        mInfoTextView.setText(R.string.turn_computer);
+                        int move = mGame.GetComputerMove();
+                        SetMove(mGame.GetComputerPlayer(), move);
+
+                        winner = mGame.CheckForWinner();
+                    }
+
+                    if(winner == TicTacToeGame.TypeOfWinner.GameNotOver)
+                    {
+                        mInfoTextView.setText(R.string.turn_human);
+                    }
+                    else if(winner == TicTacToeGame.TypeOfWinner.Tie)
+                    {
+                        mInfoTextView.setText(R.string.result_tie);
+                        mTiesCounter++;
+                        mTiesCount.setText(Integer.toString(mTiesCounter));
+                        mGameOver = true;
+                    }
+                    else if(winner == TicTacToeGame.TypeOfWinner.PlayerWins)
+                    {
+                        mInfoTextView.setText(R.string.result_human_wins);
+                        mPlayerCounter++;
+                        mPlayerCount.setText(Integer.toString(mPlayerCounter));
+                        mGameOver = true;
+                    }
+                    else
+                    {
+                        mInfoTextView.setText(R.string.result_android_wins);
+                        mAndroidCounter++;
+                        mAndroidCount.setText(Integer.toString(mAndroidCounter));
+                        mGameOver = true;
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    private void SetMove(char player, int location)
+    {
+        mGame.SetMove(player, location);
+        mBoardButtons[location].setEnabled(false);
+        mBoardButtons[location].setText(String.valueOf(player));
+
+        if(player == mGame.GetHumanPlayer())
+            mBoardButtons[location].setTextColor(Color.GREEN);
+        else
+            mBoardButtons[location].setTextColor(Color.RED);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/tony/tictactoegame/TicTacToeGame.java
@@ -0,0 +1,179 @@
+package com.tony.tictactoegame; /**
+ * Created by Tony on 2014-09-14.
+ */
+import java.util.Random;
+
+/**
+ * Created by Tony on 2014-09-13.
+ */
+public class TicTacToeGame
+{
+
+    enum TypeOfWinner
+    {
+        PlayerWins,
+        CompWins,
+        Tie,
+        GameNotOver
+    }
+
+    //variables
+    private final int mBoardDim = 3;
+    private char[] mBoard;
+    private final char mPlayer = 'X';
+    private final char mComp = 'O';
+    private final char mEmptySpace = ' ';
+    private Random mRand;
+
+    //methods
+
+    //constructor
+    TicTacToeGame()
+    {
+        mBoard = new char[9];
+
+        for(int i = 0; i < mBoardDim; i++)
+        {
+            mBoard[i] = mEmptySpace;
+        }
+
+        mRand = new Random();
+    }
+
+    public final char GetHumanPlayer()
+    {
+        return mPlayer;
+    }
+
+    public final char GetComputerPlayer()
+    {
+        return mComp;
+    }
+
+
+
+    public int GetBoardSize()
+    {
+        return mBoardDim;
+    }
+
+    public void ClearBoard()
+    {
+        for(int i = 0; i < mBoardDim; ++i)
+        {
+            mBoard[i] = mEmptySpace;
+        }
+    }
+
+    public void SetMove(char player,int location)
+    {
+        mBoard[location] = player;
+    }
+
+    public int GetComputerMove()
+    {
+        int move;
+
+        //computer checks to see if there are any locations that will allow it to win
+        for(int i = 0; i < mBoardDim; ++i)
+        {
+            //if space is empty
+            if(mBoard[i] != mPlayer && mBoard[i] != mComp)
+            {
+                char curr = mBoard[i];
+                mBoard[i] = mComp;
+
+                //if CheckForWinner == 3 the computer has won and sets it move to that position
+                if(CheckForWinner() == TypeOfWinner.CompWins)
+                {
+                    SetMove(mComp, i);
+                    return i;
+                }
+                //set back to a empty position
+                else
+                {
+                    mBoard[i] = curr;
+                }
+            }
+
+        }
+
+
+        for(int i = 0; i < mBoardDim; ++i)
+        {
+            //if space is empty
+            if(mBoard[i] != mPlayer && mBoard[i] != mComp)
+            {
+                char curr = mBoard[i];
+                mBoard[i] = mPlayer;
+                //if CheckForWinner == 2 the human player will win and
+                // the computer sets it move to that position to stop it
+                if(CheckForWinner() == TypeOfWinner.PlayerWins)
+                {
+                    SetMove(mComp, i);
+                    return i;
+                }
+                //set back to a empty position
+                else
+                {
+                    mBoard[i] = curr;
+                }
+            }
+
+        }
+
+        do
+        {
+
+            move = mRand.nextInt(mBoardDim);
+        }while(mBoard[move] == mPlayer || mBoard[move] == mComp);
+
+        SetMove(mComp, move);
+
+        return move;
+    }
+
+
+
+    public TypeOfWinner CheckForWinner()
+    {
+
+        //check winner horizontally
+        for(int i = 0;i < 6; i+= 3)
+        {
+            if(mBoard[i] == mPlayer &&  mBoard[i+1] == mPlayer && mBoard[i+2] == mPlayer)
+                return TypeOfWinner.PlayerWins;
+
+            if(mBoard[i] == mComp && mBoard[i+1] == mComp && mBoard[i+2] == mComp)
+                return TypeOfWinner.CompWins;
+        }
+
+        //check winner vertically
+        for(int i = 0; i <= 2; ++i)
+        {
+            if(mBoard[i] == mPlayer && mBoard[i+3] == mPlayer && mBoard[i+6] == mPlayer)
+                return TypeOfWinner.PlayerWins;
+            if(mBoard[i] == mComp && mBoard[i+3] == mComp && mBoard[i+6] == mComp)
+                return TypeOfWinner.CompWins;
+        }
+
+        //check winner diagonally
+        if((mBoard[0] == mPlayer && mBoard[4] == mPlayer && mBoard[8] == mPlayer)
+         || (mBoard[2] == mPlayer && mBoard[4] == mPlayer && mBoard[6] == mPlayer))
+            return TypeOfWinner.PlayerWins;
+
+        if((mBoard[0] == mComp && mBoard[4] == mComp && mBoard[8] == mComp)
+          ||( mBoard[2] == mComp && mBoard[4] == mComp && mBoard[6] == mComp))
+            return TypeOfWinner.CompWins;
+
+        //return 0 if there is still a space
+        for(int i = 0; i < mBoardDim; ++i)
+        {
+            if(mBoard[i] != mPlayer && mBoard[i] != mComp)
+                return TypeOfWinner.GameNotOver;
+        }
+
+        //return 1 for a tie
+        return TypeOfWinner.Tie;
+    }
+}
